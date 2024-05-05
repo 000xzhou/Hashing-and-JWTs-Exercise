d437e29efbe9ff522fa845100aab603aa993ced4
000xzhou <xzhou92@gmail.com>
2024-05-05 16:18:22 -0400

    auth route

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const ExpressError = require("../expressError");
 
 /** POST /login - login: {username, password} => {token}
  *
@@ -12,9 +13,9 @@ router.post("/login", async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const result = await User.authenticate(username, password);
-    let token = jwt.sign({ result }, process.env.SECRET_KEY);
+    let token = jwt.sign({ username }, process.env.SECRET_KEY);
     User.updateLoginTimestamp(username);
-    return res.json(token);
+    return res.json({ token: token });
   } catch (err) {
     return next(err);
   }
@@ -28,17 +29,20 @@ router.post("/login", async (req, res, next) => {
 router.post("/register", async (req, res, next) => {
   try {
     const { username, password, first_name, last_name, phone } = req.body;
-    const result = await User.register(
+    const result = await User.register({
       username,
       password,
       first_name,
       last_name,
-      phone
-    );
-    let token = jwt.sign({ result }, process.env.SECRET_KEY);
+      phone,
+    });
+    let token = jwt.sign({ username }, process.env.SECRET_KEY);
     User.updateLoginTimestamp(username);
-    return res.json(token);
+    return res.json({ token: token });
   } catch (err) {
+    if (err.code === "23505") {
+      return next(new ExpressError(`Username already exist`, 400));
+    }
     return next(err);
   }
 });
